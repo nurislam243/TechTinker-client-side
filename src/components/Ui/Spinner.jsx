5f8a2d10eecf3e334dfbd2accb1db815e1566b29
nurislam243 <mdnurislam15177@gmail.com>
2025-07-01 19:53:33 +0600

    update spinner

--- src/components/Ui/Spinner.jsx
+++ src/components/Ui/Spinner.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Spinner = () => {
-    return (
-        <div className='absolute top-[350px] lg:top-[380px] w-full flex justify-center z-50'>
-            <span class="loader"></span>
-        </div>
-    );
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      <span className="loader"></span>
+    </div>
+  );
 };
 
 export default Spinner;
