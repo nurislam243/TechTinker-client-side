7bc195170e6b615910d3110b6255509e49374412
nurislam243 <mdnurislam15177@gmail.com>
2025-07-02 07:07:55 +0600

    added sorting functionality in all services page

--- src/pages/Services/AllServices.jsx
+++ src/pages/Services/AllServices.jsx
@@ -9,6 +9,31 @@ const AllServices = () => {
     const [searchText, setSearchText] = useState('');
 
 
+    // sorting functionality
+    const handleSort = (order) => {
+        if (order === 'default') {
+            setServices(data); // original unsorted data
+        } else if (order === 'asc') {
+            const sorted = [...services].sort((a, b) => a.price - b.price);
+            setServices(sorted);
+        } else if (order === 'desc') {
+            const sorted = [...services].sort((a, b) => b.price - a.price);
+            setServices(sorted);
+        } else if (order === 'az') {
+            const sorted = [...services].sort((a, b) =>
+            a.serviceName.localeCompare(b.serviceName)
+            );
+            setServices(sorted);
+        } else if (order === 'za') {
+            const sorted = [...services].sort((a, b) =>
+            b.serviceName.localeCompare(a.serviceName)
+            );
+            setServices(sorted);
+        }
+    };
+
+
+
     // handle search functionality
     useEffect(() => {
         const searchedServices = data.filter(service => service.serviceName.toLowerCase().includes(searchText.toLowerCase()));
@@ -26,14 +51,32 @@ const AllServices = () => {
                 </h2>
                 <p className="text-base md:text-lg mt-2 text-base-content/50 max-w-xl mx-auto">Find the perfect tech solution for your device — from repairs to upgrades, trusted by experts near you.</p>
             </div>
-            <div className="mb-12 mt-3 flex items-center justify-center">               
-                <input 
-                    type="text"
-                    placeholder="Search by Service Name"
-                    className="input w-99"
-                    onChange={e=>setSearchText(e.target.value)}
-                />             
+
+            <div className="">
+                {/* search */}
+                <div className="mb-12 mt-3 flex items-center justify-center">               
+                    <input 
+                        type="text"
+                        placeholder="Search by Service Name"
+                        className="input w-99"
+                        onChange={e=>setSearchText(e.target.value)}
+                    />             
+                </div>
+                {/* sort */}
+                <div className="flex items-center justify-start @min-[1586px]:justify-end mb-7 -mt-3 @min-[1586px]:mr-[70px] gap-2">
+                    <p>Sort By: </p>
+                    <select className="border p-1.5 rounded" onChange={(e) => handleSort(e.target.value)}>
+                        <option value="default">Default</option>
+                        <option value="asc">Price: Low to High</option>
+                        <option value="desc">Price: High to Low</option>
+                        <option value="az">Name: A to Z</option>
+                        <option value="za">Name: Z to A</option>
+                    </select>
+                </div>
+
             </div>
+
+            {/* services card */}
             <div className="space-y-6 sm:space-y-7 md:space-y-8 lg:space-y-9">
                 {
                     services.map(service => <ServiceCard service={service}></ServiceCard>)
